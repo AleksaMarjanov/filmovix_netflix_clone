4113450c1e8623ea10fcfa44097d353ad7fcf633
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-07-04 20:49:40 +0000

    add isMobileIcon & ThemeProvider

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,8 +1,21 @@
 import { makeStyles } from '@mui/styles';
 
-export default makeStyles(() => ({
-  height: '80px',
-  display: 'flex',
-  justifyContent: 'space-between',
-  marginLeft: '240px',
+export default makeStyles((theme) => ({
+  toolbar: {
+
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginLeft: '240px',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 0,
+      flexWrap: 'wrap',
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
 }));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App.jsx';
 
+const theme = createTheme({});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
