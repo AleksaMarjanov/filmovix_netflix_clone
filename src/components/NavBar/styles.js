81b1aa6018e79efce7f7d1b9ef929391d9a61686
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-04 15:31:19 -0500

    add styles

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  height: '80px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginLeft: '240px',
+}));
