2bf8a7fb85c1b7df640b48afbb03925fc9982201
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-09 17:45:31 -0500

    style movieInformation comp

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -4,14 +4,17 @@ import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBord
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import { useGetMovieQuery } from '../../services/TMDB';
 
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { useGetMovieQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
 import useStyles from './styles';
 
 const MovieInformation = () => {
   const { id } = useParams();
   const { data, isFetching, error } = useGetMovieQuery(id);
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   if (isFetching) {
     return (
@@ -41,6 +44,21 @@ const MovieInformation = () => {
       <Grid item container direction="column" lg={7}>
         <Typography variant="h3" align="center" gutterBottom>{data?.title} ({data.release_date.split('-')[0]})</Typography>
         <Typography variant="h5" align="center" gutterBottom>{data?.tagline}</Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography variant="subtitle1" gutterBottom style={{ marginLeft: '10px' }}>{data?.vote_average}</Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>{data?.runtime}min  {data?.spoken_languages.length > 0 ? `/${data?.spoken_languages[0].name}` : ''}</Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link key={genre.name} className={classes.links} to="/" onClick={() => dispatch(selectGenreOrCategory(genre.id))}>
+              <img src={genreIcons[genre.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color="textPrimary" variant="subtitle1">{genre?.name}</Typography>
+            </Link>
+          ))}
+        </Grid>
       </Grid>
     </Grid>
   );
