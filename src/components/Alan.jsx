f3d0dbd9d474ff52a68913fa51d9b540ef6efc7b
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-14 18:48:30 -0500

    add login/logout to AlanAI

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
@@ -16,6 +17,12 @@ const useAlan = () => {
           } else {
             setMode('dark');
           }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'handleLogout') {
+          localStorage.clear();
+
+          window.location.href = '/';
         }
       },
     });
